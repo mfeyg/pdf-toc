337d569a937c1a4096bb57db1c284dd12106bcf3
Andrew Johnson <andrewbrettjohnson@gmail.com>
2015-06-23 04:32:08 -0500

    Adjust indentation and braces

--- pdf-toc.cpp
+++ pdf-toc.cpp
@@ -6,27 +6,31 @@
 
 using namespace std;
 
-bool has_extension(char *filename, char *extension) {
-  int offset = strlen(filename) - strlen(extension);
-  return offset >= 0 && strcmp(filename + offset, extension) == 0;
+bool has_extension(char *filename, char *extension)
+{
+    int offset = strlen(filename) - strlen(extension);
+    return offset >= 0 && strcmp(filename + offset, extension) == 0;
 }
 
-void check_arguments(int argc, char *argv[]) {
-  if (argc == 5
-      && has_extension(argv[1], ".pdf")
-      && has_extension(argv[2], ".txt")
-      && strcmp(argv[3], "-o") == 0
-      && has_extension(argv[4], ".pdf")) {
-    return;
-  }
-  else {
-      cout << "Usage: " << argv[0] << " in.pdf toc.txt -o out.pdf" << endl;
-      exit(EXIT_FAILURE);
-  }
-
+void check_arguments(int argc, char *argv[])
+{
+    if (argc == 5
+        && has_extension(argv[1], ".pdf")
+        && has_extension(argv[2], ".txt")
+        && strcmp(argv[3], "-o") == 0
+        && has_extension(argv[4], ".pdf"))
+    {
+        return;
+    }
+    else
+    {
+        cout << "Usage: " << argv[0] << " in.pdf toc.txt -o out.pdf" << endl;
+        exit(EXIT_FAILURE);
+    }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     check_arguments(argc, argv);
     ifstream in_pdf(argv[1]);
     ifstream toc_txt(argv[2]);
