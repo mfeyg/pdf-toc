bad65f1aba1e21c66d77b9df1c7bc960da7695fd
Mendel Feygelson <mfeygelson@gmail.com>
2015-06-28 19:07:10 -0500

    use Boost::Spirit to parse TOC

--- TOC.cpp
+++ TOC.cpp
@@ -1,75 +1,74 @@
 #include <string>
 #include <vector>
 #include <iostream>
-using namespace std;
+#include <iterator>
 
-struct Entry {
-  string name;
-  int page;
-  vector<Entry> children;
-  Entry(string name, int page, vector<Entry> children)
-    : name(name), page(page), children(children) {}
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+
+namespace spirit = boost::spirit;
+namespace qi = spirit::qi;
+namespace ascii = spirit::ascii;
+namespace phoenix = boost::phoenix;
+
+
+struct TOC {
+  std::string name;
+  int pages;
+  std::vector<TOC> children;
 };
 
-struct Line {
-  int depth;
-  string name;
-  int page;
-  Line(int depth, string name, int page)
-    : depth(depth), name(name), page(page) {}
+BOOST_FUSION_ADAPT_STRUCT(
+  TOC,
+  (std::string, name)
+  (int, pages)
+  (std::vector<TOC>, children)
+)
+
+template <typename Iterator>
+struct TOC_grammar : qi::grammar<Iterator, TOC(), qi::locals<int> >
+{
+  TOC_grammar() : TOC_grammar::base_type(toc) {
+    using qi::eps;
+    using qi::lit;
+    using qi::int_;
+    using ascii::char_;
+    using qi::eol;
+    using qi::repeat;
+    using qi::inf;
+    using qi::omit;
+    using namespace qi::labels;
+
+    count %= eps[_val = 0] >> *(lit(_r1)[_val += 1]);
+
+    name %= +(char_ - '\t');
+
+    toc %= omit[count('\t')[_a = _1]]
+         > name
+         > +lit('\t')
+         > int_
+         > eol
+         > children(_a+1);
+
+    children %= *(&repeat(_r1,inf)[lit('\t')] >> toc);
+  }
+
+  qi::rule<Iterator, TOC(), qi::locals<int> > toc;
+  qi::rule<Iterator, std::vector<TOC>(int)> children;
+  qi::rule<Iterator, std::string()> name;
+  qi::rule<Iterator, int(char)> count;
 };
-
-Line parseLine(const string& line) {
-  int tabs = 0;
-  while (line[tabs] == '\t') ++tabs;
-  int last_tab = line.rfind('\t');
-  int page_no = stoi(line.substr(last_tab + 1));
-  string name(line, tabs, last_tab - tabs);
-  return Line(tabs, string(name, 0, name.find_last_not_of('\t')+1), page_no);
-}
-
-int readLines(istream& in, vector<Line>& out) {
-  string line;
-  int count = 0;
-  while (getline(in, line)) {
-    out.push_back(parseLine(line));
-    ++count;
-  }
-  return count;
-}
-
-vector<Line>::iterator readSection(vector<Line>::iterator in,
-                                   vector<Line>::iterator end,
-                                   vector<Entry>& out) {
-  int depth = in->depth;
-  while (in != end && in->depth == depth) {
-    string name = in->name;
-    int page = in->page;
-    ++in;
-    vector<Entry> children;
-    if (in->depth > depth)
-      in = readSection(in, end, children);
-    out.push_back(Entry(name, page, children));
-  }
-  return in;
-}
-
-void printTOC(ostream& out, const Entry& toc) {
-  out << "Name: '" << toc.name << "' Page: " << toc.page << " Children: [";
-  for (auto it = toc.children.begin(); it != toc.children.end(); ++it) {
-    printTOC(out, *it);
-  }
-  out << "], ";
-}
 
 int main() {
-  vector<Line> lines;
-  readLines(cin, lines);
-  vector<Entry> toc;
-  readSection(lines.begin(), lines.end(), toc);
-  cout << toc.size() << endl;
-  for (auto it = toc.begin(); it != toc.end(); ++it) {
-    printTOC(cout, *it);
-    cout << endl;
-  }
+  TOC_grammar<spirit::istream_iterator> grammar;
+  spirit::istream_iterator begin(std::cin);
+  spirit::istream_iterator end;
+  TOC toc;
+  bool res;
+  std::cin.unsetf(std::ios::skipws); // No white space skipping!
+  res = qi::parse(begin, end, TOC_grammar<spirit::istream_iterator>(), toc);
+  if (res) std::cout << "Success\n"; else std::cout << "Failure\n";
+  std::cout << toc.name << ';' << toc.pages << ';' << toc.children.size() << std::endl;
 }
